c5baf6266afee466a59136060138f07e5b808c56
jonathan-saunders <jonsaunders43@gmail.com>
2024-10-01 14:43:10 -0300

    cap fps to 60

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     n = 5
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +16,7 @@ def main():
                 return
         pygame.Surface.fill(screen, color=0)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 #ensures the main function is only called when this file
 #is run directly; it won't run if it's imported as a module
